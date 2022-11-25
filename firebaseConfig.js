acc04c3426f396712859924c06ba5c572bf6a431
skipper1931 <lordskipper1931@gmail.com>
2022-11-25 03:25:10 -0500

    Reformat per ESLint rules

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDDRmVPKQl5stKINZX3JkV8Fakc4s3Ddk",
-  authDomain: "webdesign-salon.firebaseapp.com",
-  projectId: "webdesign-salon",
-  storageBucket: "webdesign-salon.appspot.com",
-  messagingSenderId: "990302414733",
-  appId: "1:990302414733:web:b022f02a0fd21153f2ffc4"
-};
+    apiKey: 'AIzaSyBDDRmVPKQl5stKINZX3JkV8Fakc4s3Ddk',
+    authDomain: 'webdesign-salon.firebaseapp.com',
+    projectId: 'webdesign-salon',
+    storageBucket: 'webdesign-salon.appspot.com',
+    messagingSenderId: '990302414733',
+    appId: '1:990302414733:web:b022f02a0fd21153f2ffc4'
+}
 
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)

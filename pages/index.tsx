973ce37d35506c10683872aec6da6534f7ba375d
Rafael Almeida <rafaelpbcp@gmail.com>
2022-12-01 14:02:12 -0500

    Added auth navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,28 @@
+import Link from "next/link";
+import { auth } from "../firebaseConfig";
+
 export default function Home() {
-  return <p>Home page</p>;
+  function signOutHandler() {
+    auth.signOut();
+  }
+
+  return (
+    <>
+      <p>Home page</p>
+      <Link href="/login">
+        <p>Login</p>
+      </Link>
+      <Link href="/signUp">
+        <p>Sign Up</p>
+      </Link>
+      {auth.currentUser && (
+        <>
+          <p>Logged in as: {auth.currentUser.email}</p>
+          <button className="btn btn-primary" onClick={signOutHandler}>
+            Logout
+          </button>
+        </>
+      )}
+    </>
+  );
 }

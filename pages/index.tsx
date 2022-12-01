fe5e806356f33af6637998e7d5f3d84f9e300f86
Aaron Perrotta <aperrotta@ucvts.org>
2022-12-01 14:31:28 -0500

    Firestore test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
+import { app } from "../firebaseConfig";
+import { getFirestore, collection, getDocs, QuerySnapshot, DocumentData } from "firebase/firestore";
+import { useState } from "react";
+
 export default function IndexPage() {
-  return <p>Home page</p>;
+  const [query, setQuery] = useState<QuerySnapshot<DocumentData> | null>(null);
+  const db = getFirestore(app);
+
+  getDocs(collection(db, "test")).then(
+    function(result) {
+      setQuery(result);
+    },
+    function(error) {
+      throw error;
+    }
+  );
+
+  return (<>
+    <h1>Hello World</h1>
+
+    <p>{query?.docs[0].data().Name}</p>
+    <p>{query?.docs[1].data().Name}</p>
+  </>);
 }

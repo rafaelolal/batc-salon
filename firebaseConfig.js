009eedb775b78d561cf9a22db98d06095d8f3db2
Aaron Perrotta <aperrotta@ucvts.org>
2022-12-01 22:36:38 -0500

    Move db object from index.tsx to firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,3 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Export Firestore object
+export const db = getFirestore(app);

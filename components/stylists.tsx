8a65f1faeb3fa7aec84335729d1ca5b497c2b037
Rafael Almeida <rafaelpbcp@gmail.com>
2022-12-13 13:30:15 -0500

    feat: added stylists page

---
+++ components/stylists.tsx
@@ -0,0 +1,10 @@
+const stylistImages = [
+  "/images/stylists/stylist1.jpg",
+  "/images/stylists/stylist2.jpg",
+  "/images/stylists/stylist4.jpg",
+  "/images/stylists/stylist3.jpg",
+];
+
+export default function Stylists() {
+  
+}

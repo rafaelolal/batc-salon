2d2b4a7cb738379a7b50979bf7612d3f5ca47600
Aaron Perrotta <aperrotta@ucvts.org>
2022-12-14 11:47:04 -0500

    Autolint _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 // In `pages/_document.js`
-import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -16,6 +16,6 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
 

58beaf05315a184c70a3436416290aa260343e79
Aaron Perrotta <aperrotta@ucvts.org>
2022-12-14 11:45:36 -0500

    Move bootstrap <Script/> tag to _document.tsx to satisfy https://nextjs.org/docs/messages/no-before-interactive-script-outside-document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,10 @@ import type { AppProps } from "next/app";
 import { AppWrapper } from "../context/state";
 import Navbar from "../components/layout/navbar";
 import Footer from "../components/layout/footer";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AppWrapper>
-      <Script
-        type="text/javascript"
-        src="js/bootstrap.bundle.min.js"
-        strategy="beforeInteractive"
-      />
       <Navbar />
       <Component {...pageProps} />
       <Footer />

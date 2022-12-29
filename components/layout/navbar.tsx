56852a06ab8a95cc730a245ab1e1f4c152ae7764
Rafael Almeida <rafaelpbcp@gmail.com>
2022-12-29 11:30:00 -0500

    feat: new navbar

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
+import FacebookIcon from "../icons/facebook";
+import InstagramIcon from "../icons/instagram";
+import PinterestIcon from "../icons/pinterest";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
           Navbar
         </a>
-
         <button
-          className="navbar-toggler"
+          class="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
@@ -17,37 +19,24 @@ export default function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
         </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" aria-current="page" href="#">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">
                 HOME
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
                 SERVICES
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                GALLERY
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                FAQs
-              </a>
-            </li>
-            <li className="nav-item">
-              <Link href="/dashboard">
-                <a className="nav-link">DASHBOARD</a>
-              </Link>
-            </li>
           </ul>
+          <PinterestIcon />
+          <FacebookIcon />
+          <InstagramIcon />
         </div>
       </div>
     </nav>

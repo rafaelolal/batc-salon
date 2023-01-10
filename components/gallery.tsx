c26a8e9db97fd008a03faed9282fc66d5c7281be
Aaron Perrotta <aperrotta@ucvts.org>
2023-01-10 17:24:07 -0500

    Linter fixes

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,7 +1,7 @@
 import { storage } from "../firebaseConfig";
 import { getDownloadURL, listAll, ref } from "firebase/storage";
 import Image from "next/image";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function Gallery() {
   const galleryImgsRef = ref(storage, "gallery-imgs");
@@ -9,20 +9,20 @@ export default function Gallery() {
 
   useEffect(() => {
     const getImages = async () => {
-      let imgs = await listAll(galleryImgsRef);
-      let imgs_jsx: JSX.Element[] = [];
+      const imgs = await listAll(galleryImgsRef);
+      const imgs_jsx: JSX.Element[] = [];
       for(const img of imgs.items) {
         const url = await getDownloadURL(img);
-        imgs_jsx.push(<Image src={url} width="250px" height="400px"/>);
+        imgs_jsx.push(<Image src={url} alt="A previous customer with an amazing haircut" width="250px" height="400px"/>);
       }
 
       setImages(imgs_jsx);
-    }
+    };
 
     getImages();
-  }, [setImages]);
+  }, [setImages, galleryImgsRef]);
   
   return <>
     {images}
-  </>
+  </>;
 }

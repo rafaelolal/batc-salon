fde965d8a250df8d4e0f348b9b24a8c44b8f3e88
Aaron Perrotta <aperrotta@ucvts.org>
2023-01-11 09:42:43 -0500

    Moved gallery image fetching to server-side (may move back)

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,27 +1,15 @@
-import { storage } from "../firebaseConfig";
-import { getDownloadURL, listAll, ref } from "firebase/storage";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
-export default function Gallery() {
-  const galleryImgsRef = ref(storage, "gallery-imgs");
-  const [images, setImages] = useState<JSX.Element[]>([]);
+type GalleryProps = {
+  imgUrls: string[];
+}
 
-  useEffect(() => {
-    const getImages = async () => {
-      const imgs = await listAll(galleryImgsRef);
-      const imgs_jsx: JSX.Element[] = [];
-      for(const img of imgs.items) {
-        const url = await getDownloadURL(img);
-        imgs_jsx.push(<Image src={url} alt="A previous customer with an amazing haircut" width="250px" height="400px"/>);
-      }
+export default function Gallery(props: GalleryProps) {
+  const images: JSX.Element[] = [];
+  props.imgUrls.forEach((url) => {
+    images.push(<Image src={url} alt="A previous customer with an amazing haircut" width="250px" height="400px"/>);
+  });
 
-      setImages(imgs_jsx);
-    };
-
-    getImages();
-  }, [setImages, galleryImgsRef]);
-  
   return <>
     {images}
   </>;

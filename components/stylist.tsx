41b04e1a412dd82c5d7f657a0aec62f1935447c6
Aaron Perrotta <aperrotta@ucvts.org>
2023-01-12 13:45:16 -0500

    Linter fix

--- components/stylist.tsx
+++ components/stylist.tsx
@@ -7,12 +7,12 @@ export type StylistProps = {
 
 export default function Stylist(props: StylistProps) {
   return <div style={{ margin: 10, display: "inline-block" }}>
-      <Image
-        alt={`A picture of stylist ${props.name}`}
-        width="200"
-        height="200"
-        src={`/images/stylists/${props.img}`}
-      />
-      <p>{props.name}</p>
+    <Image
+      alt={`A picture of stylist ${props.name}`}
+      width="200"
+      height="200"
+      src={`/images/stylists/${props.img}`}
+    />
+    <p>{props.name}</p>
   </div>;
 }

757ca3fc685b69cca0cc87ed50ecd4c238c79d36
Aaron Perrotta <aperrotta@ucvts.org>
2023-01-23 14:45:47 -0500

    Make gallery scroll

--- components/viewGalleryButton.tsx
+++ components/viewGalleryButton.tsx
@@ -4,24 +4,57 @@ export default function ViewGalleryButton() {
   const [ currentlyViewing, setCurrentlyViewing ] = useState(false);
 
   const toggle = useCallback(() => {
-    let new_class;
-    let old_class;
+    let new_class: string;
+    let old_class: string;
+    let new_z: number;
+    let new_position: string;
+    let animation_time: number;
+
     if(currentlyViewing) {
       new_class = "-animated-shown";
       old_class = "-animated-hidden";
+      new_z = -1;
+      new_position = "fixed";
+      animation_time = 0;
       setCurrentlyViewing(false);
     }
     else {
       old_class = "-animated-shown";
       new_class = "-animated-hidden";
+      new_z = 0;
+      new_position = "static";
+      animation_time = 1000;
       setCurrentlyViewing(true);
     }
 
     const index_top = document.getElementById("index-top");
     const index_bottom = document.getElementById("index-bottom");
-    if(index_top != null && index_bottom != null) { // Should always be true
-      index_top.classList.replace(`top${old_class}`, `top${new_class}`);
-      index_bottom.classList.replace(`bottom${old_class}`, `bottom${new_class}`);
+    if(index_top != null && index_bottom != null) { // Should always be true, but needed to satisfy TS
+      if(currentlyViewing) {
+        index_top.hidden = false;
+        index_bottom.hidden = false;
+      }
+
+      setTimeout(() => {
+        index_top.classList.replace(`top${old_class}`, `top${new_class}`);
+        index_bottom.classList.replace(`bottom${old_class}`, `bottom${new_class}`);
+      }, 1); // Short delay needed so that the animation runs after setting hidden to false above
+
+      const gallery = document.getElementById("index-background");
+      if(gallery != null && gallery.style != null) { // Should always be true, but needed to satisfy TS
+        setTimeout(() => {
+          gallery.style.zIndex = new_z.toString();
+          gallery.style.position = new_position;
+
+          if(!currentlyViewing) {
+            index_top.hidden = true;
+            index_bottom.hidden = true;
+          }
+
+          // @ts-ignore (instant is a valid value for behavior but TS doesn't see it that way for some reason)
+          window.scrollTo({top: 0, left: 0, behavior: "instant"});
+        }, animation_time);
+      }
     }
   }, [currentlyViewing, setCurrentlyViewing]);
 

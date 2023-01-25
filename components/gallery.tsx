3cd85ceed414e176de070078b12701c339b4104a
Aaron Perrotta <aperrotta@ucvts.org>
2023-01-25 14:47:32 -0500

    Style gallery a bit

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -5,10 +5,10 @@ const NUM_IMGS = 54;
 export default function Gallery() {
   const images: JSX.Element[] = [];
   for(let i = 0; i < NUM_IMGS; i++) {
-    images.push(<Image key={i} src={`/images/gallery/${i}.jpg`} alt="A previous customer with an amazing haircut" width="250px" height="400px"/>);
+    images.push(<Image className="m-1" key={i} src={`/images/gallery/${i}.jpg`} alt="A previous customer with an amazing haircut" width="250px" height="400px"/>);
   }
 
-  return <>
+  return <div className="d-flex justify-content-center flex-wrap">
     {images}
-  </>;
+  </div>;
 }

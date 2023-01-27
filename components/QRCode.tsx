46696974821dc1846fd00a6b0e4af0d6be944e34
dlewis123 <80225948+dlewis123@users.noreply.github.com>
2023-01-27 08:09:49 -0500

    made the qr code more responsive

--- components/QRCode.tsx
+++ components/QRCode.tsx
@@ -13,10 +13,10 @@ export default function QRCode(props: { data: QRCodePropsType }) {
     >
       <div className="col-11 col-md-7 mx-auto my-auto py-5">
         <div className="bg-light mx-auto my-auto container p-3">
-          <div className="row">
-            <div className="col-12 col-md-3">
+          <div className="row justify-content-center">
+            <div className="col-auto d-flex p-0">
               <Image
-                className="position-relative"
+                className="mx-auto my-auto"
                 src="/images/qrcode.png"
                 alt="alt 1"
                 height="220"
@@ -26,8 +26,8 @@ export default function QRCode(props: { data: QRCodePropsType }) {
                 blurDataURL="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg"
               />
             </div>
-            <div className="col my-auto">
-              <h1 className="text-secondary mb-0">{props.data.title}</h1>
+            <div className="col-12 col-lg-7 col-xl-auto text-center text-lg-start my-auto text-break">
+              <h2 className="text-secondary mb-0">{props.data.title}</h2>
               <small className="fs-6">{props.data.subTitle}</small>
               <p className="text-secondary fs-4 mt-3">{props.data.body}</p>
             </div>

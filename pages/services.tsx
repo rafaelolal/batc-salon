789aabf04b1d14bfb762e80cf43c5364bc24ec0a
dlewis123 <80225948+dlewis123@users.noreply.github.com>
2023-01-27 08:10:25 -0500

    started styling the service page

--- pages/services.tsx
+++ pages/services.tsx
@@ -11,46 +11,102 @@ type ServicesProps = {
   offering1: SectionPropsType;
   offering2: SectionPropsType;
   offering3: SectionPropsType;
-}
+};
 
 export default function ServicesPage(props: ServicesProps) {
-  return <>
-    <h1>Services</h1>
+  return (
+    <>
+      <h1 className="mt-5 pt-4 text-center">Services</h1>
+      <img className="d-flex mx-auto mb-4" src="/images/swirly.png"></img>
 
-    <div className="bg-primary">
-      <h2>{props.section1.title}</h2>
-      <p>{props.section1.body}</p>
-      <Image alt={props.section1.title} width="500px" height="200px" src={props.section1.imgAddr}/>
-    </div>
+      <div className=" bg-primary px-5">
+        <div
+          className="row row-cols-1 row-cols-md-2 p-5 mx-5"
+          style={{ marginBottom: "150px" }}
+        >
+          <div className="col">
+            <h2 className="text-light">What out costumers</h2>
+            <h2 className="text-light">
+              are <span className="text-secondary">Loving !</span>
+            </h2>
 
-    <h2>{props.section2.title}</h2>
-    <div className="bg-primary">
-      <p>{props.section2.body}</p>
-      <Image alt={props.section1.title} width="500px" height="200px" src={props.section2.imgAddr}/>
-      <h2>We also offer</h2>
-    </div>
+            <p className="text-light">{props.section1.body}</p>
+          </div>
 
-    <div className="row">
-      <div className="col">
-        <Image alt={props.offering1.title} width="500px" height="200px" src={props.offering1.imgAddr}/><br/>
-        <b>{props.offering1.title}</b>
-        <p>{props.offering1.body}</p>
+          <div className="col">
+            <Image
+              alt={props.section1.title}
+              width="500px"
+              height="200px"
+              src={props.section1.imgAddr}
+            />
+          </div>
+        </div>
       </div>
 
-      <div className="col">
-        <Image alt={props.offering2.title} width="500px" height="200px" src={props.offering2.imgAddr}/><br/>
-        <b>{props.offering2.title}</b>
-        <p>{props.offering2.body}</p>
+      <h2 className="text-primary ps-5 ms-5">{props.section2.title}</h2>
+      <div className="bg-primary position-relative" style={{ height: "350px" }}>
+        <div className="row row-cols-1 row-cols-md-2 ps-5 pt-2 ">
+          <div className="col">
+            <p className="text-light">{props.section2.body}</p>
+          </div>
+          <div className=" position-absolute end-0" style={{ bottom: "70%" }}>
+            <Image
+              alt={props.section1.title}
+              width="500px"
+              height="200px"
+              src={props.section2.imgAddr}
+            />
+          </div>
+        </div>
       </div>
 
-      <div className="col">
-        <Image alt={props.offering3.title} width="500px" height="200px" src={props.offering3.imgAddr}/><br/>
-        <b>{props.offering3.title}</b>
-        <p>{props.offering3.body}</p>
+      <div
+        className="bg-primary position-relative"
+        style={{ height: "200px", marginBottom: "30%" }}
+      >
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 mx-5 position-absolute">
+          <div className="col text-center">
+            <Image
+              alt={props.offering1.title}
+              width="500px"
+              height="200px"
+              src={props.offering1.imgAddr}
+            />
+            <br />
+            <b>{props.offering1.title}</b>
+            <p>{props.offering1.body}</p>
+          </div>
+
+          <div className="col text-center">
+            <Image
+              alt={props.offering2.title}
+              width="500px"
+              height="200px"
+              src={props.offering2.imgAddr}
+            />
+            <br />
+            <b>{props.offering2.title}</b>
+            <p>{props.offering2.body}</p>
+          </div>
+
+          <div className="col text-center">
+            <Image
+              alt={props.offering3.title}
+              width="500px"
+              height="200px"
+              src={props.offering3.imgAddr}
+            />
+            <br />
+            <b>{props.offering3.title}</b>
+            <p>{props.offering3.body}</p>
+          </div>
+        </div>
       </div>
-    </div>
-    <Footer/>
-  </>;
+
+      <Footer />
+    </>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
@@ -66,7 +122,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
     section2: props_untyped["services_2"] as SectionPropsType,
     offering1: props_untyped["offering_1"] as SectionPropsType,
     offering2: props_untyped["offering_2"] as SectionPropsType,
-    offering3: props_untyped["offering_3"] as SectionPropsType
+    offering3: props_untyped["offering_3"] as SectionPropsType,
   };
 
   return { props };
